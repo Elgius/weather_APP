6c22dfd7f44a8b78f9a967f7cb4261330044e005
Elgius <114131653+Elgius@users.noreply.github.com>
2022-10-09 13:48:32 +0500

    patch 1
    - updated readme
    - added comments into the code base so anyone who reads will know what is happening

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # weather_APP
- weather app basically that runs on command line
+## Rough summary on how this app works:
+
+weather app basically that runs on command line
 
 its is to be noted that we will be using the OpenWeather API and have limited callbacks. i am not disclosing API key, but if you are to use this program, please create a seperate file or enter your API key into the respective fields on the main program file
 
-it is also to be noted that the program creates a json file and uses fill up a F string to give details of the weather
-
-not, there is no GUI as of yet, probably coming soon 
+it is also to be noted that the program creates a json file and filters it to to display key information on the weather. Does that mean this app works? yes, but that also means normal people who are not used to python dictionaries or json files will have a hard time to decipher what the data is
--- test.py
+++ test.py
@@ -9,22 +9,30 @@ BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 def Url(city):
 
+    #Builds the URL and shoots it into browser thanks to the requests module
     URL = f"{BASE_URL}?q={city}&appid={API_KEY}"
     response = requests.get(URL)
     weather_info = response.json
     data = response.content
 
+    #Saves the data as a Json file
     with open("data.json", "wb") as f:
         f.write(data)
 
+
+    #opens the Json file for data extraction by converting the json into a dictionary
     file = open("data.json", "r")
     json_data = json.load(file)
 
     print(type(json_data))
 
-    for key, value in json_data.items():
-        print(f"\n key: {key}")
-        print(f"Data: {value} \n")
+    #this code displays all the data at once
+    #for key, value in json_data.items():
+    #    print(f"\n key: {key}")
+    #    print(f"Data: {value} \n")
+
+    print(json_data["name"])
+    print(json_data["main"])
 
 
     file.close()
