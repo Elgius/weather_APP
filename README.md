66ac73d81684916a865d5f47644b306e295f3ea2
Elgius <114131653+Elgius@users.noreply.github.com>
2022-10-09 13:19:53 +0500

    json test
    Json file reading finally successful

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
  weather app basically that runs on command line
 
 its is to be noted that we will be using the OpenWeather API and have limited callbacks. i am not disclosing API key, but if you are to use this program, please create a seperate file or enter your API key into the respective fields on the main program file
+
+it is also to be noted that the program creates a json file and uses fill up a F string to give details of the weather
+
+not, there is no GUI as of yet, probably coming soon 
--- test.py
+++ test.py
@@ -9,31 +9,24 @@ BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 def Url(city):
 
-    URL = f"{BASE_URL}?q={city}&appid={API_KEY}"
-    response = requests.get(URL)
-    weather_info = response.json
-
-    f = open(response.json, "r")
-    data = json.loads(f)
-
-    print(weather_info)
-
-
-    #if weather_info['cod'] == 200:
-    #    kelvin = 273
-
-    #temp = int (weather_info["main"]["temp"])
-    #Feels_like_temp= int (weather_info["main"]["Feels_like"])
-    #pressure=(weather_info["main"]["Pressure"] )
-    #Humidity=(weather_info["main"]["Humidity"] )
-    #Wind_speed = (weather_info["main"]["Wind_speed"] * 3.6 )
-    #sunrise = (weather_info["sys"]["Sunrise"])
-    #sunset = (weather_info["sys"]["Sunset"])
+    #URL = f"{BASE_URL}?q={city}&appid={API_KEY}"
+    #response = requests.get(URL)
+    #weather_info = response.json
+    #data = response.content
 #
-    #
-    #weather = f"\nWeather of: {city}\nTemperature (Celsius): {temp}°\nFeels like in (Celsius): {Feels_like_temp}°\nPressure: {pressure} hPa\nHumidity: {Humidity}%"
-#
-    #print(weather)
+    #with open("data.json", "wb") as f:
+    #    f.write(data)
+
+    file = open("data1.json", "r")
+    json_data = json.load(file)
+
+    print(type(json_data))
+
+    for key, value in json_data.items():
+        print(f"\n key: {key}")
+        print(f"Data: {value} \n")
+
+
 
 
 city = input("enter the city name you want to search up on?:  ")
