057b5de097e2e4a09e9b808cb37a3a50f6c4973a
Elgius <114131653+Elgius@users.noreply.github.com>
2022-10-11 12:33:45 +0500

    Basic GUI interface
    GUI time...still got lot of work to be done

--- test.py
+++ test.py
@@ -1,13 +1,27 @@
+import string
 from archive import key
 import json
 import requests
+from tkinter import *
+
+
+root =Tk()
+root.geometry("400x400")
+root.resizable(0,0) 
+
+root.title("Weather App")
 
 
 API_KEY = key
 
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
-def Url(city):
+city_value = StringVar()
+
+def Url():
+
+    
+    city = city_value.get()
 
     #Builds the URL and shoots it into browser thanks to the requests module
     URL = f"{BASE_URL}?q={city}&appid={API_KEY}"
@@ -31,30 +45,42 @@ def Url(city):
     #    print(f"\n key: {key}")
     #    print(f"Data: {value} \n")
 
-    print("What data are you interested in viewing?")
-    print("1. Temperature")
-    print("2. feel like temperature")
-    print("3. Pressure")
-    print("TBA")
-    print("TBA")
-    print("TBA")
-    print("TBA")
-    print("TBA")
+    #print("What data are you interested in viewing?")
+    #print("1. Temperature")
+    #print("2. feel like temperature")
+    #print("3. Pressure")
+    #print("TBA")
+    #print("TBA")
+    #print("TBA")
+    #print("TBA")
+    #print("TBA")
 
-    choice = input("enter required data:  ")
+    results = str(json_data["main"]["temp"])
 
-    match choice:
-        case "1" : print(json_data["main"]["temp"])
-        case "2":  print(json_data["main"]["feels_like"])
-        case "3" : print(json_data["main"]["pressure"])
-        case _:print("Enter the correct value or the data requested not available")
+    print(results)
 
+    tfield.insert = (INSERT, results)
 
     file.close()
 
 
 
 
-city = input("enter the city name you want to search up on?:  ")
+city_head= Label(root, text = 'Enter City Name', font = 'Arial 12 bold').pack(pady=10) #to generate label heading
+ 
+inp_city = Entry(root, textvariable = city_value,  width = 24, font='Arial 14 bold').pack()
+ 
+ 
+Button(root, command = Url, text = "Check Weather", font="Arial 10", bg='lightblue', fg='black', activebackground="teal", padx=5, pady=5 ).pack(pady= 20)
+ 
+#to show output
+ 
+weather_now = Label(root, text = "The Weather is:", font = 'arial 12 bold').pack(pady=10)
+ 
+tfield = Text(root, width=46, height=10)
 
-Url(city)
+tfield.pack()
+
+root.mainloop()
+
+
